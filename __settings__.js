056eddeef97019aa26d837557494164e30d99c51
PureProz <131679129+PureProz@users.noreply.github.com>
2024-03-22 08:29:35 -0500

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1972177.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'preferWebGpu': false,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 172129877, 172129841, 172129829, 172129875, 172129848, 172129869 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
